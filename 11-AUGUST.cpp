5e23924621023300fe2a72c63f0770543a8d07db
Divyansh Nakhate <138779643+divyansh02n@users.noreply.github.com>
2023-08-11 23:23:31 +0530

    Create 11-AUGUST.cpp

---
+++ 11-AUGUST.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    long long int count(int coins[], int N, int sum) {
+
+        // code here.
+        long long dp[N+1][sum+1];
+        for(int i=0;i<=sum;i++)
+        {
+            dp[0][i]=0;
+        }
+         for(int i=0;i<=N;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(coins[i-1]>j)
+                 dp[i][j]=dp[i-1][j];
+                 
+                 else
+                 dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+                 
+                 
+            }
+        }
+           
+        
+        
+        return dp[N][sum];
+    }
+};
